f7a131e5e519fe6ab6444339e5222923c3f816ce
bcyt <brianychin@gmail.com>
2020-07-19 18:11:09 -0500

    static fortune using express

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+var express = require('express');
+var app = express();
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Fortune of the day coming soon :)\n');
+app.get('/', function(req, res) {
+  res.send('Random fortune coming soon');
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
 });
